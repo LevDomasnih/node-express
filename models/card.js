e8b432a1c600d1604aa1dc7d8e187c0f06e47a07
lev <lev.domasnix2013@gmail.com>
2021-08-27 00:41:26 +0500

    Add pay course

--- models/card.js
+++ models/card.js
@@ -1,10 +1,51 @@
-class Card {
-    add() {
+const fs = require('fs')
+const path = require('path')
 
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'card.json'
+)
+
+class Card {
+    static async add(course) {
+        const card = await Card.fetch()
+
+        const idx = card.courses.findIndex(c => c.id === course.id)
+        const candidate = card.courses[idx]
+
+        if (candidate) {
+            candidate.count++
+            card.courses[idx] = candidate
+        } else {
+            course.count = 1
+            card.courses.push(course)
+        }
+
+        card.price += +card.price
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(p, JSON.stringify(card), (err) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve()
+                }
+            })
+        })
     }
 
-    fetch() {
-
+    static async fetch() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(p,"utf-8", (err, content) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(JSON.parse(content))
+                    }
+                }
+            )
+        })
     }
 }
 
