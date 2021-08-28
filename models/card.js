1347f53d003c243e09315203ad0e75cc23aa89ad
lev <lev.domasnix2013@gmail.com>
2021-08-28 23:59:14 +0500

    Add delete card item

--- models/card.js
+++ models/card.js
@@ -22,7 +22,7 @@ class Card {
             card.courses.push(course)
         }
 
-        card.price += +card.price
+        card.price += +course.price
 
         return new Promise((resolve, reject) => {
             fs.writeFile(p, JSON.stringify(card), (err) => {
@@ -30,6 +30,32 @@ class Card {
                     reject(err)
                 } else {
                     resolve()
+                }
+            })
+        })
+    }
+
+    static async remove(id) {
+        const card = await Card.fetch()
+
+        const idx = card.courses.findIndex(c => c.id === id)
+        const course = card.courses[idx]
+
+        if (course.count === 1) {
+            card.courses = card.courses.filter(e => e.id !== id)
+        } else {
+            card.count = 1
+            card.courses[idx].count--
+        }
+
+        card.price -= course.price
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(p, JSON.stringify(card), (err) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(card)
                 }
             })
         })
