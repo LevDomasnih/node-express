c7c1fd961be5775e99f3488b1cb3f40b29531b1e
lev <lev.domasnix2013@gmail.com>
2021-08-29 01:50:01 +0500

    Refactoring course model

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = 'mongodb+srv://lev:KWBbnxtKciaeiuOH@cluster0.0pbti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    const url = 'mongodb+srv://lev:KWBbnxtKciaeiuOH@cluster0.0pbti.mongodb.net/shop'
     await mongoose.connect(url, {
       useNewUrlParser: true,
     })
--- routes/add.js
+++ routes/add.js
@@ -16,11 +16,18 @@ router.post('/', async (req, res) => {
     img
   } = req.body
 
-  const course = new Course(title,price,img)
+  const course = new Course({
+    title,
+    price,
+    img
+  })
 
-  await course.save()
-
-  res.redirect('/courses')
+  try {
+    await course.save()
+    res.redirect('/courses')
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 
