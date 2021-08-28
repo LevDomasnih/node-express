9a7b38bf90fa2a99da25246f7a8791e4501a650a
lev <lev.domasnix2013@gmail.com>
2021-08-29 00:57:46 +0500

    Add rerender card

--- public/app.js
+++ public/app.js
@@ -1,9 +1,15 @@
+const toCurrency = (price) => {
+    return new Intl.NumberFormat('ru-RU', {
+      currency: 'rub',
+      style: 'currency'
+    }).format(price)
+}
+
 document.querySelectorAll('.price').forEach(node => {
-  node.textContent = new Intl.NumberFormat('ru-RU', {
-    currency: 'rub',
-    style: 'currency'
-  }).format(node.textContent)
+  node.textContent = toCurrency(node.textContent)
 })
+
+
 
 const $card = document.querySelector('#card')
 if ($card) {
@@ -15,6 +21,23 @@ if ($card) {
         method: 'delete'
       })
           .then(res => res.json())
+          .then(card => {
+            if (card.courses.length) {
+              const html = card.courses.map(c => {
+                return `
+                    <tr>
+                        <td>${c.title}</td>
+                        <td>${c.count}</td>
+                        <td><button class="btn btn-small js-remove" data-id="${c.id}">Delete</button></td>
+                    </tr>
+                `
+              }).join('')
+              $card.querySelector('tbody').innerHTML = html
+              $card.querySelector('.price').textContent = toCurrency(card.price)
+            } else {
+              $card.innerHTML = `<p>Card is empty</p>`
+            }
+          })
     }
   })
 }
