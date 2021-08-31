c523b83ebbc86fda98161da429cabc643fc046ed
LevDomasnih <lev.domasnix2013@gmail.com>
2021-08-31 21:52:24 +0500

    Сохранение сессии

--- index.js
+++ index.js
@@ -26,16 +26,6 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('612d11b47108f906ace10202')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
@@ -60,17 +50,17 @@ async function start() {
     await mongoose.connect(url, {
       useNewUrlParser: true,
     })
-    const candidate = await User.findOne()
-    if (!candidate) {
-      const user = new User({
-        email: 'lev@gmail.ru',
-        name: 'Lev',
-        cart: {
-          items: []
-        }
-      })
-      await user.save()
-    }
+    // const candidate = await User.findOne()
+    // if (!candidate) {
+    //   const user = new User({
+    //     email: 'lev@gmail.ru',
+    //     name: 'Lev',
+    //     cart: {
+    //       items: []
+    //     }
+    //   })
+    //   await user.save()
+    // }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
