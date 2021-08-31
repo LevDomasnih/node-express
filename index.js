96a1a5b6dc8ebb995490e054057bd2295d44a860
LevDomasnih <lev.domasnix2013@gmail.com>
2021-08-31 22:07:05 +0500

    Сессия в базе данных

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
 const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
-const User = require('./models/user')
 const varMiddleware = require('./middleware/variables')
+
+const MONGODB_URI = 'mongodb+srv://Lev:mNehQApmw0BXlvzu@cluster0.jjnmg.mongodb.net/shop'
 
 const app = express()
 
@@ -20,6 +22,10 @@ const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
   handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -31,7 +37,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: 'some secret value',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }))
 app.use(varMiddleware)
 
@@ -46,21 +53,9 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = 'mongodb+srv://Lev:mNehQApmw0BXlvzu@cluster0.jjnmg.mongodb.net/shop'
-    await mongoose.connect(url, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
     })
-    // const candidate = await User.findOne()
-    // if (!candidate) {
-    //   const user = new User({
-    //     email: 'lev@gmail.ru',
-    //     name: 'Lev',
-    //     cart: {
-    //       items: []
-    //     }
-    //   })
-    //   await user.save()
-    // }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
