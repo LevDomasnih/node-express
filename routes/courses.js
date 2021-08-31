0e481919064b44af855b03e8d6759a87569a2c8c
LevDomasnih <lev.domasnix2013@gmail.com>
2021-08-31 22:13:43 +0500

    Защита роутов

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
+const auth = require('../middleware/auth')
 
 router.get('/', async (req, res) => {
   const courses = await Course.find()
@@ -14,7 +15,7 @@ router.get('/', async (req, res) => {
   })
 })
 
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', auth, async (req, res) => {
   const course = await Course.findById(req.params.id)
   if (!req.query.allow) {
     return res.redirect('/')
@@ -26,7 +27,7 @@ router.get('/:id/edit', async (req, res) => {
   })
 })
 
-router.post('/remove', async (req, res) => {
+router.post('/remove', auth, async (req, res) => {
   try {
   await Course.deleteOne({_id: req.body.id})
     res.redirect('/courses')
@@ -35,7 +36,7 @@ router.post('/remove', async (req, res) => {
   }
 })
 
-router.post('/edit', async (req, res) => {
+router.post('/edit', auth, async (req, res) => {
   const {id} = req.body
   delete req.body.id
   await Course.findByIdAndUpdate(id, req.body)
