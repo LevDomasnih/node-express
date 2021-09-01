c711bef6ed2d81d18ef0b1310ae9c04d301f5fae
LevDomasnih <lev.domasnix2013@gmail.com>
2021-09-01 23:54:51 +0500

    Улучшение данных

--- utils/validators.js
+++ utils/validators.js
@@ -1,23 +1,30 @@
-const {body} = require('express-validator')
-const User = require('../models/user')
+const { body } = require("express-validator")
+const User = require("../models/user")
 
 exports.registerValidators = [
-  body('email', 'Enter valid email').isEmail().custom(async (value, {req}) => {
-    try {
-      const user = await User.findOne({email: value})
-      if (user) {
-        return Promise.reject('Email has already')
+  body("email", "Enter valid email").
+    isEmail().
+    custom(async (value, { req }) => {
+      try {
+        const user = await User.findOne({ email: value })
+        if (user) {
+          return Promise.reject("Email has already")
+        }
+      } catch (e) {
+        console.log(e)
       }
-    } catch (e) {
-      console.log(e)
-    }
-  }),
-  body('password', 'Min password is 6 char').isLength({min: 6, max: 56}).isAlphanumeric(),
-  body('confirm').custom((value, {req}) => {
+    }).
+    normalizeEmail(),
+
+  body("password", "Min password is 6 char").
+    isLength({ min: 6, max: 56 }).isAlphanumeric().trim(),
+
+  body("confirm").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error('passwords must match')
+      throw new Error("passwords must match")
     }
     return true
-  }),
-  body('name', 'Min name is 3 char').isLength({min: 3})
+  }).trim(),
+
+  body("name", "Min name is 3 char").isLength({ min: 3 }).trim(),
 ]
