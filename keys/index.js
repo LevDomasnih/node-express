4b61eae2536af9534006828c2817acde186e43d9
LevDomasnih <lev.domasnix2013@gmail.com>
2021-09-02 22:43:44 +0500

    Защита ключей

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://Lev:mNehQApmw0BXlvzu@cluster0.jjnmg.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.prod')
 }
---
+++ keys/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://Lev:mNehQApmw0BXlvzu@cluster0.jjnmg.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value'
+}
